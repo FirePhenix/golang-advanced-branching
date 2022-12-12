06b372018949bcb87a67de5db2bb914a3466ac20
Michael Hu <michael.hus@outlook.com>
2022-12-12 13:52:53 -0500

    update

--- vehicle.go
+++ vehicle.go
@@ -35,6 +35,8 @@ var inventory []vehicle
 
 // Model array for the feedback.json file
 type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
 }
 type rating float32
 
